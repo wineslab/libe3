f20048ff500c31cb5e9add48b14a763aec0e848d
Andrea Lacava <a.lacava@northeastern.edu>
2026-03-02 15:53:31 -0500

    feat: Add optional JSON encoding support and mutual exclusion with ASN.1

--- src/encoder/encoder_factory.cpp
+++ src/encoder/encoder_factory.cpp
@@ -6,19 +6,26 @@
  */
 
 #include "libe3/e3_encoder.hpp"
+#if LIBE3_ENABLE_ASN1
 #include "asn1_encoder.hpp"
+#endif
+#if LIBE3_ENABLE_JSON
 #include "json_encoder.hpp"
+#endif
 #include "libe3/logger.hpp"
 
 namespace libe3 {
 
 std::unique_ptr<E3Encoder> create_encoder(EncodingFormat format) {
     switch (format) {
+#if LIBE3_ENABLE_ASN1
         case EncodingFormat::ASN1:
             return std::make_unique<Asn1E3Encoder>();
-        
+#endif
+#if LIBE3_ENABLE_JSON
         case EncodingFormat::JSON:
             return std::make_unique<JsonE3Encoder>();
+#endif
         
         default:
             return nullptr;
